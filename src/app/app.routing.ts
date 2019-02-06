91831248d4737abf1c7c208e2f3cd00d42f584ef
jonathan rojas <jonathangrh.25@gmail.com>
2019-02-06 09:51:51 -0500

    se mejoran las rutas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const appRoutes: Routes = [
-    { path: '', redirectTo: '/home' , pathMatch: 'full' }
-];
+const appRoutes: Routes = [];
 
 export const appRoutingProviders: any[] = [];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
